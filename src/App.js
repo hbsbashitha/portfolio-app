2d69b8709a0e1b7f1d351bf17bd01b9a55ed73d1
Bhashitha Ranasinghe <73308863+hbsbashitha@users.noreply.github.com>
2023-03-03 12:42:11 +0530

    tst

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <main>
           <Header />
           <LandingSection />
-          <ProjectsSection />
+//           <ProjectsSection />
           <ContactMeSection />
           <Footer />
           <Alert />
